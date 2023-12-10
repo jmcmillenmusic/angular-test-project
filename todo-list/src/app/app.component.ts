e4f08e128a1a99838e34ab79134a067dc1acec33
Jeff McMillen <jmcmillenmusic@gmail.com>
2023-12-10 15:36:27 -0600

    Creating functionality to remove tasks and updating functions on buttons to show array changes

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -18,7 +18,25 @@ export class AppComponent {
     'Wash the dishes', 
     'Shop for the party'
   ];
+
   add(newTask: string) {
     this.tasks.push(newTask);
-  }
+  };
+
+  remove(existingTask: string) {
+    var userConfirmed = confirm(`Are you sure you want to remove this task? \n "${existingTask}"`);
+    if (userConfirmed) {
+      this.tasks = this.tasks.filter(task => task != existingTask);
+    }
+  };
+
+  markAsDone(task: string) {
+    alert('This task is done: ' + task);
+  };
+}
+
+class Tank {
+  constructor(public title: string) {
+    
+  };
 }
